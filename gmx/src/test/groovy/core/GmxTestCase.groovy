5500e051b47e5cdaa872d0b239b0518cd087550b
Nicholas Whitehead <nwhitehead@heliosdev.org>
2012-01-04 18:20:17 -0500

    Fixed groovy logging.

--- gmx/src/test/groovy/core/GmxTestCase.groovy
+++ gmx/src/test/groovy/core/GmxTestCase.groovy
@@ -3,10 +3,30 @@ import groovy.util.GroovyTestCase;
 import java.lang.management.*;
 import org.junit.*;
 import org.junit.rules.*;
+import org.apache.log4j.*;
 
 
 class GmxLocalTestCase extends GroovyTestCase {
+	/** The pid of the current VM */
 	def pid = ManagementFactory.getRuntimeMXBean().getName().split("@")[0];
+	
+	/** Tracks the test name */
+	@Rule
+    def testName= new TestName();
+    
+	/** Instance logger */
+	def LOG = Logger.getLogger(getClass());
+    
+    static {
+    	BasicConfigurator.configure();
+   	}
+    
+	@Before
+	void setUp() {		
+		String methodName = getMethodName();
+		LOG.debug("\n\t******\n\t Test [" + getClass().getSimpleName() + "." + methodName + "]\n\t******");
+	}
+	
 	
     void testGroovyLocalDomain()  {
     	def localDomain = ManagementFactory.getPlatformMBeanServer().getDefaultDomain();
