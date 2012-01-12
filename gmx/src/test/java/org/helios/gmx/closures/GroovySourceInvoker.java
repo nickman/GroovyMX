a12d52d2165e34d9b5050a30eea6be6d7eade484
Nicholas Whitehead <nwhitehead@heliosdev.org>
2012-01-12 13:52:03 -0500

    Basic command line java to load and execute groovy source.

---
+++ gmx/src/test/java/org/helios/gmx/closures/GroovySourceInvoker.java
@@ -0,0 +1,64 @@
+/**
+ * Helios, OpenSource Monitoring
+ * Brought to you by the Helios Development Group
+ *
+ * Copyright 2007, Helios Development Group and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org. 
+ *
+ */
+package org.helios.gmx.closures;
+
+import groovy.lang.GroovyShell;
+import groovy.lang.Script;
+
+import java.io.File;
+
+import org.helios.gmx.classloading.ReverseClassLoader;
+
+/**
+ * <p>Title: GroovySourceInvoker</p>
+ * <p>Description: </p> 
+ * <p>Company: Helios Development Group LLC</p>
+ * @author Whitehead (nwhitehead AT heliosdev DOT org)
+ * <p><code>org.helios.gmx.closures.GroovySourceInvoker</code></p>
+ */
+public class GroovySourceInvoker {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		log("GroovySourceInvoker");
+		GroovyShell shell = new GroovyShell();
+		//ReverseClassLoader.getInstance();
+		try {
+			//Script script = shell.parse(new File("./src/test/groovy/core/RemoteBlockCounts.groovy"));
+			Script script = shell.parse(new File("./src/test/groovy/core/OTServerRemote.groovy"));
+			script.run();
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+		}
+
+	}
+	
+	public static void log(Object msg) {
+		System.out.println(msg);
+	}
+
+}
